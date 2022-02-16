a7fb652fbbedc3e9141d24feac72e39cbfc38bfa
Amalu Sajeev <amalu.sajeev.me@gmail.com>
2022-02-17 00:23:38 +0530

    documentation helper

---
+++ utils/SwaggerDoc.js
@@ -0,0 +1,44 @@
+class DocsAPIMethod {
+  constructor(summary, description = ``) {
+    this.summary = summary;
+    this.description = description;
+    this.parameters = [];
+  }
+  set addParam(parameter) {
+    this.parameters.push(parameter);
+  }
+}
+
+class DocsAPIParam {
+  constructor(where, name, required) {
+    this.in = where;
+    this.name = name;
+    this.required = required;
+  }
+  set addSchema([title, description]) {
+    this.schema = new DocsParamSchema(title, description);
+  }
+}
+
+class DocsParamSchema {
+  constructor(title, description) {
+    this.title = title;
+    this.description = description;
+    this.properties = {};
+  }
+  set prop([name, type, def]) {
+    this.properties[name] = {};
+    this.properties[name].type = type;
+    this.properties[name].default = def;
+  }
+}
+
+class DocsSchemaProp {
+  constructor(name, type = "string", def = "") {
+    this.name = name;
+    this.type = type;
+    this.default = def;
+  }
+}
+
+export { DocsAPIMethod, DocsAPIParam, DocsParamSchema, DocsSchemaProp };
