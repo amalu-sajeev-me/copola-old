601818195514bf5716dd6b0296103615d1664100
Amalu Sajeev <amalu.sajeev.me@gmail.com>
2022-02-17 00:51:41 +0530

    docs improvements

---
+++ bin/docs/docs.style.js
@@ -0,0 +1,6 @@
+const styles = `
+.swagger-ui .topbar .topbar-wrapper, .swagger-ui .topbar a{
+    display: none;
+}`;
+
+export { styles };
--- middlewares/swagger-ui-express.js
+++ middlewares/swagger-ui-express.js
@@ -1,7 +1,13 @@
 import swaggerUI from "swagger-ui-express";
 import { swaggerDocument } from "../bin/docs/index.js";
+import { styles } from "../bin/docs/docs.style.js";
 
+const options = {
+  customCss: styles,
+};
+
+console.log(styles);
 const swaggerServe = swaggerUI.serve;
-const swaggerSetup = swaggerUI.setup(swaggerDocument);
+const swaggerSetup = swaggerUI.setup(swaggerDocument, options);
 
 export { swaggerServe, swaggerSetup };
